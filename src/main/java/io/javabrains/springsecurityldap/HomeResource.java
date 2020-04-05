28cec2aa5f6225975043e0164d0fee9973f78f09
visweswa <visweswa@adobe.com>
2020-04-05 22:26:08 +0530

    ldap changes

---
+++ src/main/java/io/javabrains/springsecurityldap/HomeResource.java
@@ -0,0 +1,14 @@
+package io.javabrains.springsecurityldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home() {
+        return ("<h1>Home Page</h1>");
+    }
+}
+
--- src/test/java/io/javabrains/springsecurityldap/SpringSecurityLdapApplicationTests.java
+++ src/test/java/io/javabrains/springsecurityldap/SpringSecurityLdapApplicationTests.java
@@ -1,4 +1,4 @@
-package io.javabrains.springsecurityjpa;
+package io.javabrains.springsecurityldap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringSecurityJpaApplicationTests {
+public class SpringSecurityLdapApplicationTests {
 
 	@Test
 	public void contextLoads() {
